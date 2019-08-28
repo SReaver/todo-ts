5af4cd370f4767cd32d3b16d08f81634fba2c7f5
Reaver <reaver@Slava-Mac.local>
2019-08-28 23:29:10 +0600

    CRD

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import axios, { AxiosResponse } from 'axios';
 import { Todo } from './Todo';
 
 interface IState {
-  id: string
+  id: string;
   text: string;
   done: boolean;
   todos: ITodo[];
 }
-interface IProps { };
+interface IProps {}
 class App extends React.Component<IProps, IState> {
   state: IState = {
     id: '',
@@ -19,60 +19,80 @@ class App extends React.Component<IProps, IState> {
     todos: []
   };
   componentDidMount() {
-    console.log('mount')
+    this.getData();
   }
-  // componentDidMount() {
-  //   console.log('mount');
-  // axios
-  //   .get('https://todo-ts-58222.firebaseio.com/todos.json')
-  //   .then((response: AxiosResponse) => {
-  //     console.log(response.data);
-  //     this.setState({todos: [...Response.data]})
-  //   })
-  // }
+  getData = () => {
+    axios
+      .get('https://todo-ts-58222.firebaseio.com/todos.json')
+      .then((response: AxiosResponse) => {
+        const fetchedOrders = [];
+        for (let key in response.data) {
+          fetchedOrders.push({
+            ...response.data[key],
+            id: key
+          });
+        }
+        this.setState({ todos: [...fetchedOrders] });
+      });
+  };
+  deleteFromServer = (e: any) => {
+    axios
+      .delete(`https://todo-ts-58222.firebaseio.com/todos/${e.target.id}.json`)
+      .then(response => {})
+      .then(() => {
+        this.getData();
+      })
+      .catch(err => console.log(err));
+  };
+  delHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    this.deleteFromServer(e);
+  };
+
   onChangeHandler = (e: any) => {
     e.preventDefault();
     this.setState({ text: e.target.value });
   };
   onCheckHandler = (e: any) => {
     this.setState({ done: e.target.checked });
-    console.log(this.state.done);
   };
-  dl = () => {
-
-  }
   submitHandler = (e: any) => {
     e.preventDefault();
     const data = { text: this.state.text, done: this.state.done };
     axios
       .post('https://todo-ts-58222.firebaseio.com/todos.json', data)
       .then((response: AxiosResponse) => {
-        console.log('response.data', response.data);
         const arr = [];
-        arr.push({ id: response.data.name, text: this.state.text, done: this.state.done })
-        this.setState({ todos: [...this.state.todos, ...arr] });
-        console.log('this.state', this.state);
+        arr.push({
+          id: response.data.name,
+          text: this.state.text,
+          done: this.state.done
+        });
+        this.setState({ todos: [...this.state.todos, ...arr], text: '' });
       })
-      .catch(error => {
-        console.log(error);
-      });
+      .catch(err => console.log(err));
   };
   public render() {
     let todos = null;
     if (this.state.todos.length) {
       todos = this.state.todos.map(todo => {
-        return <Todo key={todo.id}
-          id={todo.id}
-          text={todo.text}
-          done={todo.done}
-          dl={this.dl}
-        />
-      })
+        return (
+          <Todo
+            key={todo.id}
+            id={todo.id}
+            text={todo.text}
+            done={todo.done}
+            dl={(e: React.MouseEvent<HTMLButtonElement>) => this.delHandler(e)}
+          />
+        );
+      });
     }
     return (
       <div className="App">
         <header className="App-header">
-          <form onSubmit={this.submitHandler} style={{ display: 'flex', alignItems: 'center' }}>
+          <form
+            onSubmit={this.submitHandler}
+            style={{ display: 'flex', alignItems: 'center' }}
+          >
             <input
               name="isDone"
               type="checkbox"
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 interface dl {
-  dl: () => void;
+  dl: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 export interface ITodo {
   id: string;
   text: string;
   done: boolean;
 }
-interface IProps extends ITodo, dl { }
+interface IProps extends ITodo, dl {}
 
 export const Todo = (props: IProps) => {
   return (
-    <div key={props.id} style={{ display: 'flex', width: '300px', justifyContent: 'space-between', alignItems: 'center' }}>
+    <div
+      key={props.id}
+      style={{
+        display: 'flex',
+        width: '300px',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}
+    >
       <input type="checkbox" checked={props.done} disabled />
       <span>{props.text}</span>
-      <button onClick={props.dl}>Delete</button>
+      <button
+        id={props.id}
+        onClick={(e: React.MouseEvent<HTMLButtonElement>) => props.dl(e)}
+      >
+        Delete
+      </button>
     </div>
   );
 };
